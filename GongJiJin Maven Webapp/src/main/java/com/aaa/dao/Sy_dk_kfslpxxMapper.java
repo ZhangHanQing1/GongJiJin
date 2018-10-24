efdd020bdcaa4fb2f44a43a8675f0ab5e9ea984e
ZhangHanQing1 <39086269+ZhangHanQing1@users.noreply.github.com>
2018-10-24 09:30:12 +0800

    sc

--- GongJiJin Maven Webapp/src/main/java/com/aaa/dao/Sy_dk_kfslpxxMapper.java
+++
@@ -1,9 +0,0 @@
-package com.aaa.dao;
-
-import com.aaa.entity.Sy_dk_kfslpxx;
-
-public interface Sy_dk_kfslpxxMapper {
-    int insert(Sy_dk_kfslpxx record);
-
-    int insertSelective(Sy_dk_kfslpxx record);
-}
--- GongJiJin Maven Webapp/src/main/java/com/aaa/dao/Sy_dk_yhjjjcsjMapper.java
+++
@@ -1,9 +0,0 @@
-package com.aaa.dao;
-
-import com.aaa.entity.Sy_dk_yhjjjcsj;
-
-public interface Sy_dk_yhjjjcsjMapper {
-    int insert(Sy_dk_yhjjjcsj record);
-
-    int insertSelective(Sy_dk_yhjjjcsj record);
-}
